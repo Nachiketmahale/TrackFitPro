dd3255649c0fd524678d793dd928403e283332e3
MahaleNachiket <nachiketmh7@gmail.com>
2025-01-31 05:49:24 +0530

    raw components formed

---
+++ src/app/components/user-chart/user-chart.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-chart',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-chart.component.html',
+  styleUrl: './user-chart.component.css'
+})
+export class UserChartComponent {
+
+}
